f50d133c4ed6aa00dd9eb1b30c4e5767a8a87167
Ryan Kim <thechaserg@gmail.com>
2023-06-14 20:11:33 +0800

    Stars - Q2

--- 02-loooooooooop/01_while_loop.py
+++ 02-loooooooooop/01_while_loop.py
@@ -1,101 +1,101 @@
-# While Loop
-age = 1
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # While Loop
+# age = 1
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 
-# if age < 18:
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
+# # if age < 18:
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
 	
-# # .... Until When?
-# print ("Blah Blah Blah...")
+# # # .... Until When?
+# # print ("Blah Blah Blah...")
 
-# age  = 17
-# if age < 18: # age = 17
+# # age  = 17
+# # if age < 18: # age = 17
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
+	
+# # if age < 18: # age = 18
+# # 	print("You Cannot Drink Alcohol at age:", age)
+# # 	age += 1
+# # else:
+# # 	print("You reached age",age,",Let's get your first Soju!")
+	
+# # Problem is that it is very tedious job to copy the same if statement 10 times. We must wrap redundant codes into loops.
+
+# ## while loop executes the body until the associated boolean statement returns True
+
+# while (age < 18): # while age is less than 18,
 # 	print("You Cannot Drink Alcohol at age:", age)
 # 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
 	
-# if age < 18: # age = 18
-# 	print("You Cannot Drink Alcohol at age:", age)
-# 	age += 1
-# else:
-# 	print("You reached age",age,",Let's get your first Soju!")
-	
-# Problem is that it is very tedious job to copy the same if statement 10 times. We must wrap redundant codes into loops.
+# # ...computer exits the while loop when age is NOT less than 18, starts reading here
+# print("You reached age",age,",Let's get your first Soju!")
 
-## while loop executes the body until the associated boolean statement returns True
+# # another example of while loop
+# num = 0.0
+# while num < 10.0: # This while runs while num < 10
+# 	num = num + 0.1 # This line is executed 10.0/0.1 = 100 times.
 
-while (age < 18): # while age is less than 18,
-	print("You Cannot Drink Alcohol at age:", age)
-	age += 1
-	
-# ...computer exits the while loop when age is NOT less than 18, starts reading here
-print("You reached age",age,",Let's get your first Soju!")
+# print("num is",int(num)) # num is 10
 
-# another example of while loop
-num = 0.0
-while num < 10.0: # This while runs while num < 10
-	num = num + 0.1 # This line is executed 10.0/0.1 = 100 times.
+# # Practicing while loop
 
-print("num is",int(num)) # num is 10
+# # Q1
+# # print the following.
+# # *
+# # **
+# # ***
+# # ****
 
-# Practicing while loop
+# print("instead of this...")
+# print("*")
+# print("**")
+# print("***")
+# print("****")
 
-# Q1
-# print the following.
-# *
-# **
-# ***
-# ****
-
-print("instead of this...")
-print("*")
-print("**")
-print("***")
-print("****")
-
-print("do this!")
-numStar = 1 # first row has 1 star
-while numStar <= 4:
-	print("*"*numStar)
-	numStar += 1 # for every print of stars, add 1, since for every row, the number of star increases by 1
+# print("do this!")
+# numStar = 1 # first row has 1 star
+# while numStar <= 4:
+# 	print("*"*numStar)
+# 	numStar += 1 # for every print of stars, add 1, since for every row, the number of star increases by 1
 	
 # HW: print those shapes below:
 # Q2
@@ -104,6 +104,21 @@ while numStar <= 4:
 # *****
 #  ***
 #   *
+
+num_space = 2
+num_star = 1
+count = 1
+
+
+while count <= 5:
+    print(' '*num_space+'*'*num_star)
+    if count <= 2:
+        num_space -= 1
+        num_star += 2
+    else:
+        num_space += 1
+        num_star -= 2
+    count += 1
 
 # Q3
 # *******
