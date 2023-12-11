046048c47a8de5a9ee46a09b5f79e79f729c3d1a
Dowon Hwang <hdwkr95.developer@gmail.com>
2023-12-11 13:59:25 +0900

    chore: testcases for practicing collection

--- 03-collections/03-practicing-collections.py
+++ 03-collections/03-practicing-collections.py
@@ -30,11 +30,7 @@ print("reversing the order of L1", L1.reverse())
 - Print all values.
 """
 
-dictionary = {
-    "a": "1",
-    "b": "2",
-    "c": "3"
-}
+dictionary = {"a": "1", "b": "2", "c": "3"}
 dictionary["d"] = "4"
 dictionary["b"] = "5"
 del dictionary["c"]
@@ -51,7 +47,7 @@ print("all keys in dictionary", dictionary.keys())
 - Convert the list back to a tuple.
 """
 T1 = (1, 2, 3, 4)
-L1= list(T1)
+L1 = list(T1)
 L1.append(5)
 T1 = tuple(L1)
 print(T1)
@@ -79,49 +75,24 @@ def word_count(string):
     pass
 
 
+# Test case 1
+test_string_1 = "Hello world! Hello."
+print(word_count(test_string_1))  # Expected output: {'hello': 2, 'world': 1}
+
+# Test case 2
+test_string_2 = "The quick brown fox jumps over the lazy dog."
+print(
+    word_count(test_string_2)
+)  # Expected output: {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1, 'jumps': 1, 'over': 1, 'lazy': 1, 'dog': 1}
+
+# Test case 3
+test_string_3 = "A man, a plan, a canal: Panama."
+print(
+    word_count(test_string_3)
+)  # Expected output: {'a': 3, 'man': 1, 'plan': 1, 'canal': 1, 'panama': 1}
+
 # C2. Invert a Dictionary:
 # Write a function that inverts a dictionary. The keys should become values and the values should become keys. Assume all values of the original dictionary are unique.
-
-
-def invert_dict(d):
-    # Your code here
-    pass
-
-
-# C3. Merge Two Dictionaries:
-# Write a function that merges two dictionaries. If there is a key collision, prefer the value from the second dictionary.
-
-
-def merge_dicts(d1, d2):
-    # Your code here
-    pass
-
-
-# C4. Dictionary to List of Tuples:
-# Write a function that converts a dictionary into a list of tuples, where each tuple is a key-value pair.
-
-
-def dict_to_tuples(d):
-    # Your code here
-    pass
-
-
-# C5. Group Anagrams:
-# Given a list of strings, group anagrams together. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-def group_anagrams(words):
-    # Your code here
-    pass
-
-
-# C6. Least Frequent Element:
-# Given a list, find the element that appears least frequently. Return a tuple containing the element and its count. If there's a tie, return any of them.
-def least_frequent(lst):
-    # Your code here
-    pass
-
-
-# test
-word_count("pen pineapple pen pen apple pen")
 
 baseball_terms = {
     "double play": "A double play occurs when two offensive players are ruled out within the same play.",
@@ -138,15 +109,50 @@ kendo_terms = {
     "do": "A target area in Kendo corresponding to the torso or trunk.",
 }
 
-invert_dict(baseball_terms)
-merge_dicts(baseball_terms, kendo_terms)
 
+def invert_dict(d):
+    # Your code here
+    pass
+
+
+# Test case for invert_dict
+# Expected outcome: The definitions become keys, and the terms become values.
+print(invert_dict(baseball_terms))
+
+
+# C3. Merge Two Dictionaries:
+# Write a function that merges two dictionaries. If there is a key collision, prefer the value from the second dictionary.
+
+
+def merge_dicts(d1, d2):
+    # Your code here
+    pass
+
+
+# Test case for merge_dicts
+# Expected outcome: A new dictionary with all keys and values from both, with the second dictionary's values overriding any duplicates from the first.
+print(merge_dicts(baseball_terms, kendo_terms))
+
+# C4. Dictionary to List of Tuples:
+# Write a function that converts a dictionary into a list of tuples, where each tuple is a key-value pair.
 
 keys = ["name", "age", "school", "favorite_sports"]
 values = ["Ryan", 13, "HKIS", "baseball"]
 D2 = dict(zip(keys, values))
-dict_to_tuples(D2)
-group_anagrams(["pen", "pineapple", "apple"])
+
+
+def dict_to_tuples(d):
+    # Your code here
+    pass
+
+
+# Test case for dict_to_tuples
+# Expected outcome: [('name', 'Ryan'), ('age', 13), ('school', 'HKIS'), ('favorite_sports', 'baseball')]
+print(dict_to_tuples(D2))
+
+
+# C5. Group Anagrams:
+# Given a list of strings, group anagrams together. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
 lst = [
     "apple",
@@ -160,4 +166,26 @@ lst = [
     "apple",
     "banana",
 ]
-least_frequent()
+
+
+def group_anagrams(words):
+    # Your code here
+    pass
+
+
+# Additional test case for group_anagrams
+# Expected outcome: [['bat'], ['tab', 'abt'], ['rat', 'tar', 'art'], ['can'], ['act', 'cat']]
+# Explanation: 'bat' has no anagrams in the list, 'tab', 'abt' are anagrams, 'rat', 'tar', 'art' are anagrams, 'can' has no anagrams, 'act' and 'cat' are anagrams.
+print(group_anagrams(["bat", "tab", "rat", "can", "act", "tar", "cat", "art", "abt"]))
+
+
+# C6. Least Frequent Element:
+# Given a list, find the element that appears least frequently. Return a tuple containing the element and its count. If there's a tie, return any of them.
+def least_frequent(lst):
+    # Your code here
+    pass
+
+
+# Test case for least_frequent
+# Expected outcome: Any element with the least frequency from the test list, along with its count (e.g., ('dragonfruit', 1) if 'dragonfruit' is the least frequent).
+print(least_frequent(lst))
