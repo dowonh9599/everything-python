a0d92998532360a5925379773de5bdbf27581223
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2024-05-24 18:22:06 +0800

    function scope finish

--- 04-function/P1-function-scope-practice.py
+++ 04-function/P1-function-scope-practice.py
@@ -16,7 +16,7 @@ def my_abs(num):
 
 # Question 3: Area of a Circle
 # Define a function named circle_area that computes the area of a circle. The function should take the radius of the circle as an argument and return the area.
-# Use the formula: area = π * r² (you can use 3.14159 as an approximation for π).
+# Use the formula: area = pi * r^2 (you can use 3.14159 as an approximation for pi).
 def circle_area(radius):
     pi = 3.14159
     area = pi * (radius ** 2)
@@ -39,7 +39,29 @@ def sum_list(numbers):
 # Question 6: Count Vowels
 # Define a function named count_vowels that takes a string and returns the number of vowels in that string.
 # Consider 'a', 'e', 'i', 'o', and 'u' as vowels for this exercise.
-def count_vowels
+def count_vowels(word):
+    counts = 0
+    for i in word:
+        if i == 'a':
+            counts += 1
+        if i == 'e':
+            counts += 1
+        if i == 'i':
+            counts += 1
+        if i == 'o':
+            counts += 1
+        if i == 'u':
+            counts += 1
+    return counts
+
+def count_vowels_2(word):
+    VOWELS=list("aeiou")
+    counts = sum(v in VOWELS for v in word) # summing boolean list => True = 1, False = 0
+    return counts
+
+
+print(count_vowels_2("apple"))
+
 # Question 7: Factorial
 # Write a function named factorial that computes the factorial of a given number.
 # The factorial of a non-negative integer n is the product of all positive integers less than or equal to n. For example, factorial(5) should return 120.
@@ -55,6 +77,18 @@ def factorial(number):
 # Question 8: Fibonacci Sequence
 # Create a function named fibonacci that takes a number n and returns the nth number in the Fibonacci sequence.
 # Recall that the sequence starts with 0, 1, and each subsequent number is the sum of the previous two.
+def fibonacci(n):
+    num1 = 0
+    num2 = 1
+    seq = []
+    for i in range(n):
+        # find entry for fibonacci seq by summing numbers of the previous two
+        num3 = num1 + num2
+        seq.append(num3) # append entry to the list seq
+        num1 = num2 # override num1 with num2
+        num2 = num3 # override num2 with num3
+    return seq
+print(fibonacci(5))
 
 # Question 9: Palindrome Checker
 # Define a function named is_palindrome that checks whether a given string is a palindrome
